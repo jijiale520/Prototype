f1348770db973ce9bcfe7f815598a806ed2e6604
ZhuYingJie <yanghua2@kingsoft.com>
2021-11-05 10:04:18 +0800

    原型设计模式

---
+++ Prototype/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Prototype.Properties;
+
+namespace Prototype
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+             Video v1 = new Video();
+             v1.SetName("原型模式教学");
+             v1.SetDateTime(DateTime.Now);
+             v1.SetVisionNum(1);
+             Console.WriteLine("name = {0}, datetime = {1}, hashCode = {2}, vision = {3}",
+                 v1.GetName(), v1.GetDateTime(), v1.GetHashCode(), v1.GetVisionNum());
+
+             Video v2 = v1.ShallowCopy();
+             v2.SetDateTime(DateTime.Today);
+             v2.SetVisionNum(2);
+             Console.WriteLine("name = {0}, datetime = {1}, hashCode = {2}, vision = {3}", 
+                 v2.GetName(), v2.GetDateTime(), v2.GetHashCode(), v2.GetVisionNum());
+             
+             Console.WriteLine("name = {0}, datetime = {1}, hashCode = {2}, vision = {3}", 
+                 v1.GetName(), v1.GetDateTime(), v1.GetHashCode(), v1.GetVisionNum());
+        }
+    }
+}
---
+++ Prototype/Properties/Video.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Prototype.Properties
+{
+    public class Vision
+    {
+        public int vision;
+
+        public Vision(int _vision)
+        {
+            this.vision = _vision;
+        }
+    }
+    public class Video
+    {
+        private string name;
+        private DateTime dateTime;
+        private Vision visionNum = new Vision(0);
+        public Video ShallowCopy()
+        {
+            Video obj = (Video)this.MemberwiseClone();
+            return obj;
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public void SetName(string _name)
+        {
+            this.name = _name;
+        }
+
+        public DateTime GetDateTime()
+        {
+            return dateTime;
+        }
+
+        public void SetDateTime(DateTime _date)
+        {
+            this.dateTime = _date;
+        }
+
+        public int GetVisionNum()
+        {
+            return this.visionNum.vision;
+        }
+
+        public void SetVisionNum(int _vision)
+        {
+            this.visionNum.vision = _vision;
+        }
+    }
+}
